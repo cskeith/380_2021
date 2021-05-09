33a3c35ca2751b7a4c5da04e6b5fede14034898d
lklee <cslklee@gmail.com>
2021-05-09 13:24:35 +0800

    Lab 11 project

--- src/main/resources/create_users.sql
+++ src/main/resources/create_users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     PRIMARY KEY (username)
 );
 
@@ -11,13 +11,3 @@ CREATE TABLE user_roles (
     PRIMARY KEY (user_role_id),
     FOREIGN KEY (username) REFERENCES users(username)
 );
-
-INSERT INTO users VALUES ('keith', '{noop}keithpw');
-INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_USER');
-INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_ADMIN');
-
-INSERT INTO users VALUES ('vanessa', '{noop}vanessapw');
-INSERT INTO user_roles(username, role) VALUES ('vanessa', 'ROLE_ADMIN');
-
-INSERT INTO users VALUES ('kevin', '{noop}kevinpw');
-INSERT INTO user_roles(username, role) VALUES ('kevin', 'ROLE_USER');
